7c2235c3d23257d2cd1760769c42fb21863adcd3
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-12-31 21:47:45 +0000

    Adding Admin Model code for the authentication

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const adminSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'You must provide Admin Username']
-    }
+    },
+
+    emailAddress: {
+        type: String,
+        required: [true, 'Please specify Admin e-mail address']
+    },
+    
+    password: {
+        type: String,
+        required: [true, 'Please specify password'],
+
+        validate: function(value) {
+            return this.password === value;
+        }
+    },
+
+    passwordResetToken: String,
+    passwordResetAt: Date
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
