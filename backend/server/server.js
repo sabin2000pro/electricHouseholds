6129aef10ba09402cea11bc702ecd27d8823d38e
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-12-31 17:39:09 +0000

    Added preferences model

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config({path: 'config.env'});
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const morgan = require('morgan');
