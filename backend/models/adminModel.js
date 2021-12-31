c0174db71de48480d384910a3cef32ba3091e419
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-12-31 21:49:44 +0000

    Adding Admin Model code for the authentication

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -11,7 +11,9 @@ const adminSchema = new mongoose.Schema({
 
     emailAddress: {
         type: String,
-        required: [true, 'Please specify Admin e-mail address']
+        required: [true, 'Please specify Admin e-mail address'],
+        unique: true,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please make sure your e-mail is valid']
     },
     
     password: {
@@ -27,6 +29,8 @@ const adminSchema = new mongoose.Schema({
     passwordResetAt: Date
 });
 
+// Hash Password before saving to database
+
 const Admin = mongoose.model('Admin', adminSchema);
 
-module.exports = Admin;
+module.exports = Admin; // Export the Admin model
