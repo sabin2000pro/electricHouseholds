eee0fcfeaaa1a1fac8d8339bb1b7a37e90920810
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-12-31 17:26:51 +0000

    Added admin logout component + test

---
+++ frontend/src/components/Contact/EditContact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditContact = (props) => { // Component that allows users to Edit a Contact Us form
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EditContact
--- frontend/src/hooks/useCounter.js
+++ frontend/src/hooks/useCounter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const RandomAllocation = () => {
+const useCounter = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const RandomAllocation = () => {
     )
 }
 
-export default RandomAllocation
+export default useCounter
