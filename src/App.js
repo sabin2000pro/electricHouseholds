6ca7c2386aca62a737ac2067cb1019b262d0b1f8
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-12-31 16:43:26 +0000

    Adding folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+       <h1>eHouseholds</h1>
+    </div>
+  );
+}
+
+export default App;
