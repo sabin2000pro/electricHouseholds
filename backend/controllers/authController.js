cd5ff8db3a74568ba770fcb630738643d7ec26ca
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 00:20:56 +0000

    Adding skeleton authentication controller functions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,9 +6,23 @@ const notFound = 404;
 const serverError = 500;
 
 module.exports.registerAdmin = catchAsync(async (request, response, next ) => {
-
+ return next();
 });
 
 module.exports.loginAdmin = catchAsync(async (request, response, next) => {
-    
+    return next();
+});
+
+module.exports.forgotPassword = catchAsync(async (request, response, next) => {
+
+    return next();
+});
+
+module.exports.resetAdminPassword = catchAsync(async (request, response, next) => {
+    return next();
 })
+
+module.exports.fetchAllAdmins = catchAsync(async (request, response, next) => {
+
+});
+
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const authRouter = express.Router();
 const authController = require('../controllers/authController');
 
+authRouter.route('/register-admin').post(authController.registerAdmin);
+authRouter.route('login-admin').post(authController.loginAdmin);
+authRouter.route('forgot-password').post(authController.forgotPassword);
+
 module.exports = authRouter; // Export router to be used in the server
