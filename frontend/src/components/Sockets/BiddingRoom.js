af8a36761fbddb1f94f5fd71a01b108526dfac71
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-01 18:35:16 +0000

    Adding code to the timeslot routes

---
+++ frontend/src/components/Sockets/BiddingRoom.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const BiddingRoom = (props) => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default BiddingRoom
