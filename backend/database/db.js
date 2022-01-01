096cedfd61c0413537593a6d0c4e493cf5536664
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-01 11:49:35 +0000

    Added database connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 const DB_CONN_URL = process.env.DB_CONN_URL;
 
-const connectDB = async () => {
+const connectDB = async () => { // Method to connect to the MongoDB database
     try {
-        
+        return await mongoose.connect(DB_CONN_URL).then(conn => {
+            if(conn.connection) {
+                return console.log(`Connected to DB success`);
+            }
+
+            else {
+                console.log('Could not connect to DB. Check your connection URL');
+            }
+        })
     } 
     
     catch(err) {
 
+        if(err) {
+            return console.log(error);
+        }
     }
 }
+
+module.exports = connectDB;
