fde762bc06b6df354d49a149bd3c04be2bc270f0
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-01 12:28:03 +0000

    Adding appliance model data

--- backend/models/applianceModel.js
+++ backend/models/applianceModel.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+const applianceSchema = new mongoose.Schema({
+
+});
+
+const Appliance = mongoose.model('Appliance', applianceSchema);
+module.exports = Appliance; // Export the Appliance model
