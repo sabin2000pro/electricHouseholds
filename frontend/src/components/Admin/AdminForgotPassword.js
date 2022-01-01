ed0f6e98a195508f7987db056633ec7ff3ada1c5
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 11:26:21 +0000

    Adding authentication routes

--- frontend/src/components/Admin/AdminForgotPassword.js
+++ frontend/src/components/Admin/AdminForgotPassword.js
@@ -2,10 +2,16 @@ import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 
 const AdminForgotPassword = (props) => {
+    let history = useHistory();
+
+    const [email, setEmailAddress] = useState('');
+    const [emailValid, setEmailValid] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
+
     return (
-        <div>
+        <Fragment>
             
-        </div>
+        </Fragment>
     )
 }
 
