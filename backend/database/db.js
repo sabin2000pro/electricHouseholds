1cb2f97713bda98fad6507d0bd53acbc2dfcc7ce
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 11:31:20 +0000

    Adding a database connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+const DB_CONN_URL = process.env.DB_CONN_URL;
+
+const connectDB = async () => {
+    try {
+        
+    } 
+    
+    catch(err) {
+
+    }
+}
