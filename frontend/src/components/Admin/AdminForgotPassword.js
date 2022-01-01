25eef31eace89af2761c3ce5a883d240e0fe8d22
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 11:33:07 +0000

    Adding a database connection

--- frontend/src/components/Admin/AdminForgotPassword.js
+++ frontend/src/components/Admin/AdminForgotPassword.js
@@ -1,6 +1,30 @@
 import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 
+const emailReducer = (state, action) => { // E-mail Reducer that shares state for the email and valid
+     if(action.type === 'EMAIL_SUBMIT') {
+
+     }
+
+     return {value: '', emailIsValid: false};
+}
+
+const forgotPasswordSubmitHandler = (event) => {
+    event.preventDefault();
+
+    try {
+
+    }
+    
+    catch(err) {
+
+        if(err) {
+            console.log(err);
+        }
+
+    }
+}
+
 const AdminForgotPassword = (props) => {
     let history = useHistory();
 
@@ -10,7 +34,9 @@ const AdminForgotPassword = (props) => {
 
     return (
         <Fragment>
-            
+            <form onSubmit = {forgotPasswordSubmitHandler}>
+
+            </form>
         </Fragment>
     )
 }
