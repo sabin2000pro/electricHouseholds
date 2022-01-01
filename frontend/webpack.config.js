763b6435173e203e987e8f446cbc993d8bf3065c
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 11:20:44 +0000

    Added webpack cbuild config and babel presets

---
+++ frontend/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js', // Main entry point for compilation
+
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'index_bundle.js'
+    },
+
+    module: {
+        rules: [{test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']}, {test: /.css$/, use: [ 'style-loader', 'css-loader' ]}]
+    },
+
+    plugins: [new HtmlWebpackPlugin({template: './public/index.html'})]
+
+}
