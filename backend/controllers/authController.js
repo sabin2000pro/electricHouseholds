6c0aab9afaa5536bf92b2a7ced7034754dc2d114
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 00:24:17 +0000

    Adding register auth function to controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,10 +2,19 @@ const Admin = require('../models/adminModel');
 const catchAsync = require('../utils/catchAsync');
 const ok = 200;
 const created = 201;
+const badRequest = 400;
+const unauthorized = 401;
+const forbidden = 403;
 const notFound = 404;
 const serverError = 500;
 
 module.exports.registerAdmin = catchAsync(async (request, response, next ) => {
+ const {username, emailAddress, password, confirmPassword} = request.body;
+
+ if(!validateEntries(username, emailAddress, password, confirmPassword)) {
+    return response.status()
+ }
+
  return next();
 });
 
@@ -23,6 +32,5 @@ module.exports.resetAdminPassword = catchAsync(async (request, response, next) =
 })
 
 module.exports.fetchAllAdmins = catchAsync(async (request, response, next) => {
-
+    return next();
 });
-
