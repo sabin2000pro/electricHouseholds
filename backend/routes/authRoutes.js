9307b4ec48c1aafbc1d939c7b3447fa9e8aa0517
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 11:24:48 +0000

    Adding authentication routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 authRouter.route('/register-admin').post(authController.registerAdmin);
 authRouter.route('login-admin').post(authController.loginAdmin);
 authRouter.route('forgot-password').post(authController.forgotPassword);
+authRouter.route('/reset-password').put(authController.resetAdminPassword);
 
 module.exports = authRouter; // Export router to be used in the server
