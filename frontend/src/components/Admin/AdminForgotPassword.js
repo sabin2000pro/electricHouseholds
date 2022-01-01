020bb793caaa13915810e9f7e89b342740a7bd42
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-01 00:49:46 +0000

    Added command to build a docker image

---
+++ frontend/src/components/Admin/AdminForgotPassword.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
+import {useHistory, useLocation} from 'react-router-dom';
+
+const AdminForgotPassword = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AdminForgotPassword
