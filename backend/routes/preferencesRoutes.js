aa91a21ef2789206974dbce67e477711cbee46dc
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-02 12:00:09 +0000

    Adding preference routers

--- backend/routes/preferencesRoutes.js
+++ backend/routes/preferencesRoutes.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const preferenceRouter = express.Router();
+const preferenceController = require('../controllers/preferencesController');
+
+preferenceRouter.route('/create-preference')
 
 module.exports = preferenceRouter;
