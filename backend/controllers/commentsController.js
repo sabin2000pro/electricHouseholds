6dd43e4f31a9e6c413828dba5bb698624834aa70
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-03 21:21:38 +0000

    Addingcomments model and CRUD operations on hte backend

--- backend/controllers/commentsController.js
+++ backend/controllers/commentsController.js
@@ -18,21 +18,36 @@ module.exports.createComment = catchAsync(async (request, response, next) => {
 
 module.exports.viewAllComments = catchAsync(async (request, repsonse, next) => {
     if(request.method === 'GET') {
+
         const allComments = await Comment.find();
+        return response.status(200).json({allComments});
     }
-    
+
+
 });
 
 module.exports.viewComment = catchAsync(async (request, response, next) => {
     const id = request.params.id;
+
+    if(!id) {
+
+    }
 });
 
 module.exports.editComment = catchAsync(async (request, response, next) => {
     const id = request.params.id;
+
+    if(!id) {
+
+    }
 });
 
 module.exports.deleteComment = catchAsync(async (request, response, next) => {
     const id = request.params.id;
+
+    if(!id) {
+        
+    }
 });
 
 module.exports.deleteComments = catchAsync(async (request, response, next) => {
