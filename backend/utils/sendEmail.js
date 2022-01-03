e1ecd8f26a183a17bc0a4f828b8b7247c6b72851
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-03 06:55:41 +0000

    Adding forgot and reset password logic to auth controller

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,31 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    
-});
+const sendEmail = (options) => { // Function to send e-mail
 
+    const transporter = nodemailer.createTransport({ // Create a transporter that stores the host, port and authorization parameters
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+
+    });
+
+    // Mail Options that stores the following properties
+    const mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: options.to,
+        subject: options.subject,
+        html: options.text
+    };
+
+   return transporter.sendMail(mailOptions, function(err, info) { // Send the e-mail
+      
+        
+     
+    })
+};
+
+module.exports = sendEmail; // Export function
