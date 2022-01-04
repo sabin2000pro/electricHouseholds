7b908c193a1743bfc12465d992133781db83c925
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 19:46:36 +0000

    Adding homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Homepage from './components/Home/Homepage';
+import React, {useState} from 'react';
+import HomeData from './components/data/Home.json';
 
 const App = () => {
+  const [homeData, setHomeData] = useState(HomeData);
+
   return (
 
     <div className="App">
-       <Homepage title = "eHouseholds" />
+       <Homepage data = {homeData} />
     </div>
     
   );
