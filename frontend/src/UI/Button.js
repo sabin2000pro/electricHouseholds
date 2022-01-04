0f7288606fa0a19a68f202bb9a3bb43727a31efa
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 17:33:08 +0000

    Added docker compose fi;e

--- frontend/src/UI/Button.js
+++ frontend/src/UI/Button.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types'
 
 const Button = (props) => {
     return (
-        <div>
-            
-        </div>
+        <button type = {props.type} />
     )
 }
 
