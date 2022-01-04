9706219c2810f37e23719914f4f9c54fafe9ee47
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 18:38:43 +0000

    Adding homepage

--- frontend/src/components/Home/Homepage.js
+++ frontend/src/components/Home/Homepage.js
@@ -2,19 +2,25 @@ import React, {Fragment} from 'react';
 import Header from '../Header';
 import Navigation from '../Navigation';
 import './Homepage.css';
+import HomepageImg from '../images/homepage/homepageimg.jpg';
 
 const Homepage = (props) => {
     return (
         <Fragment>
-            <section className = "section--home">
 
+            <section className = "section--home">
                 <div className = "home-text-box">
 
-                <h1>Reducing your household peak electricity consumption through Fair Negotiation Algorithms</h1>
+                <h1 className = "heading--primary">eHouseholds</h1>
+
+                <p className = "home--description">
+                    Reducing your peak electricity consumptions through Fair Negotiation Algorithms experimentation.
+                </p>
+
                 </div>
 
                 <div className = "home-img-box">
-                    
+                    <img alt = "Wind Turbing Image" src = {HomepageImg} />
                 </div>
 
             </section>
