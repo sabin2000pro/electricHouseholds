c62e4ff3dc8eb459993cd620f2b83b5da1408c41
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 18:44:12 +0000

    Adding homepage

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
 import Header from '../Header';
 import Navigation from '../Navigation';
+import {BrowserRouter as Router, Link} from 'react-router-dom'
 import './Homepage.css';
 import HomepageImg from '../images/homepage/homepageimg.jpg';
 
 const Homepage = (props) => {
     return (
         <Fragment>
+            <Router>
 
             <section className = "section--home">
                 <div className = "home-text-box">
@@ -14,16 +16,21 @@ const Homepage = (props) => {
                 <h1 className = "heading--primary">eHouseholds</h1>
 
                 <p className = "home--description">
-                    Reducing your peak electricity consumptions through Fair Negotiation Algorithms experimentation.
+                    Reducing your peak electricity consumptions through the experimentation of two Fair Negotiation Algorithms.
                 </p>
 
-                </div>
+                <Link className = "btn" to = '/'>Start Now</Link>
+                <Link className = "" to = '/'>Learn More!</Link>
+            </div>
 
                 <div className = "home-img-box">
                     <img alt = "Wind Turbing Image" src = {HomepageImg} />
                 </div>
 
             </section>
+
+            </Router>
+
 
         </Fragment>
     )
