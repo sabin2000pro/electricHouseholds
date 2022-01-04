0949a059817a49e0f9e82b366b29e280333f6716
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 11:17:27 +0000

    Adding full CRUD operations for comments

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,5 +3,8 @@ const commentRouter = express.Router();
 const commentController = require('../controllers/commentsController');
 
 commentRouter.route('/fetch-comments').get(commentController.viewAllComments);
+commentRouter.route('/create-comment').post(commentController.createComment);
+commentRouter.route('/edit-comment/:id').put(commentController.editComment);
+commentRouter.route('/delete-comment/:id').delete(commentController.deleteComment);
 
 module.exports = commentRouter;
--- backend/routes/commentsRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const commentRouter = express.Router();
-const commentController = require('../controllers/commentsController');
-
-module.exports = commentRouter;
