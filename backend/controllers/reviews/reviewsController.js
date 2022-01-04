aa1cd8a2cbf74afc257a1049b98236b5b8e51700
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 11:29:17 +0000

    Adding review model. routes and CRUD operations

--- backend/controllers/reviews/reviewsController.js
+++ backend/controllers/reviews/reviewsController.js
@@ -7,25 +7,41 @@ const notFound = 404;
 const serverError = 500;
 
 module.exports.fetchAllReviews = catchAsync(async(request, response, next) => {
-
+    if(request.method === 'GET') {
+       const allReviews = await Review.find();
+       return response.status(200).json({allReviews});
+    }
 });
 
 module.exports.fetchReviewByID = catchAsync(async (request, response, next) => {
-    
+
 })
 
 module.exports.createReview = catchAsync(async (request, response, next) => {
+    const {rating, review, username, reason} = request.body;
 
+    if(!rating || !review || !username || !reason) {
+        return response.status(400).json({status: "Fail", message: "Invalid Review Entries"});
+    }
+
+    if(request.method === 'POST') {
+        const newReview = new Review({rating, review, username, reason});
+        await newReview.save();
+
+        return response.status(201).json({newReview});
+    }
 });
 
 module.exports.editReview = catchAsync(async (request, response, next) => {
-
+    const id = request.params.id;
 });
 
 module.exports.deleteReview = catchAsync(async (request, response, next) => {
-
+    const id = request.params.id;
 });
 
 module.exports.deleteAllReviews = catchAsync(async (request, response, next) => {
-
+    if(request.method === 'DELETE') {
+        
+    }
 })
