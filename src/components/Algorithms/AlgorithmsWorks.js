7b3c232c8966f437a7aea761d0e53a6fb0f178ad
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 11:13:19 +0000

    Adding full CRUD operations for comments

--- src/components/Algorithms/AlgorithmsWorks.js
+++ src/components/Algorithms/AlgorithmsWorks.js
@@ -1,14 +1,18 @@
 import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
+import {} from 'react-icons/fa';
 import {useHistory, useLocation} from 'react-router-dom';
 import PropTypes from 'prop-types'
 
-
 const AlgorithmsWorks = (props) => { // Component that is rendered on the Algorithm page that describes how these algorithms work
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+
+        </Fragment>
     )
+}
+
+AlgorithmsWorks.defaultProps = {
+    
 }
 
 AlgorithmsWorks.propTypes = {
