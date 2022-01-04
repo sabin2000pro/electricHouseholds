c6017d5472b02a7ca55959943fb94613d087e9ab
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 11:38:06 +0000

    Adding review model. routes and CRUD operations

--- backend/routes/reviews/reviewRoutes.js
+++ backend/routes/reviews/reviewRoutes.js
@@ -2,4 +2,11 @@ const express = require('express');
 const reviewRouter = express.Router();
 const reviewController = require('../../controllers/reviews/reviewsController');
 
+reviewRouter.route('/fetch-reviews').get(reviewController.fetchAllReviews);
+reviewRouter.route('/fetch-review/:id').get(reviewController.fetchReviewByID);
+reviewRouter.route('/create-review').post(reviewController.createReview);
+reviewRouter.route('/update-review/:id').put(reviewController.editReview);
+reviewRouter.route('/delete-reviews').delete(reviewController.deleteAllReviews);
+reviewRouter.route('/delete-review/:id').delete(reviewController.deleteReview);
+
 module.exports = reviewRouter;
