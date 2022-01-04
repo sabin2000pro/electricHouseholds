d6daa475f8e1b7bce27730cdf82334a87ba82733
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-04 17:37:36 +0000

    Added docker compose fi;e

---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header';
+import Navigation from '../Navigation';
+
+const Homepage = (props) => {
+    return (
+        <div>
+            <h1>eHouseholds Homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navigation
