321aee4eb4ccabef13575271507fef7715ea3b70
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-05 11:26:47 +0000

    Adding links with React Router

--- src/components/Admin/AdminRegister.js
+++ src/components/Admin/AdminRegister.js
@@ -1,9 +1,33 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from '../../components/Header';
+import HomepageImg from '../../components/images/homepage/homepageimg.jpg';
 
 const AdminRegister = (props) => {
+    const [username, setUsername] = useState('');
     return (
          <Fragment>
-            <h1>Admin Register Page</h1>
+            <Header />
+
+<section className = "section--home">
+        <div className = "home--grid">
+
+       <div className = "home-text-box">
+
+
+         <h1 className = "heading--primary">Admin Dashboard Registration</h1>
+        <p className = "home--description">If you wish to have access to the Admin Dashboard. Please Register an account with us below and explore our options.</p>
+
+        <a className = "btn btn--full mgr-sm" href = "#">Start Now</a>
+        <a className = "btn btn--outline" href = "#">Learn More!</a>
+    </div>
+
+    <div className = "home-img-box">
+        <img className = "home--img" alt = "Wind Turbing Image" src = {HomepageImg} />
+    </div>
+</div>
+
+
+</section>
          </Fragment>
     )
 }
