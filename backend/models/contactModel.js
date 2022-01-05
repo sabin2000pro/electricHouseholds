d3c873be5f0b52e5092a3223de7c4bc2a552614d
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-05 21:56:33 +0000

    Adding Contact Us backend

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
+    firstName: {
 
+    },
+
+    lastName: {
+
+    },
+
+    username: {
+
+    },
+
+    emailAddress: {
+
+    },
+
+    issueType: {
+
+    },
+
+    description: {
+
+    }
 });
 
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
