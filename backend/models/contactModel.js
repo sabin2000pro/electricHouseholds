de9532ae9bbf568ef7bc565a9ecbdc2171929dc6
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-05 21:58:56 +0000

    Adding Contact Us backend

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -2,19 +2,23 @@ const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
     firstName: {
-
+        type: String,
+        required: [true, 'Please provide your First Name']
     },
 
     lastName: {
-
+        type: String,
+        required: [true, 'Please provide your last name']
     },
 
     username: {
-
+        type: String,
+        required: [true, 'Please provide the Username you signed up with']
     },
 
     emailAddress: {
-
+        type: String,
+        required: [true, 'Please provide the E-mail you signed up with']
     },
 
     issueType: {
