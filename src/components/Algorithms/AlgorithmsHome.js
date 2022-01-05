54a38e0417a3749ec99cf27c2ff92ea6e71bbd79
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-05 20:20:11 +0000

    Adding contact us form

--- src/components/Algorithms/AlgorithmsHome.js
+++ src/components/Algorithms/AlgorithmsHome.js
@@ -10,23 +10,26 @@ const AlgorithmsHome = (props) => {
         <Header />
 
         <section className = "section--home">
-                    <div className = "home--grid">
+             
+             <div className = "home--grid">
 
-            <div className = "home-text-box">
+             <div className = "home-text-box">
 
 
-                <h1 className = "heading--primary">Admin Dashboard Login</h1>
-                <p className = "home--description">Login into the Dashboard in order to configure appliances.</p>
+                <h1 className = "heading--primary">Negotiate your Preferences</h1>
+                <p className = "home--description">After you have submitted your timeslot preferences, please feel free to try out any of the three algorithms which are implemented primarily to persuade you to reduce your peak electricity consumption and at the same time engage in a fun environment with other users on this page</p>
 
                 <a className = "btn btn--full mgr-sm" href = "#">Start Now</a>
-                <a className = "btn btn--outline" href = "#">Learn More!</a>
+                <a className = "btn btn--outline" href = "#">About Us</a>
+
             </div>
 
                 <div className = "home-img-box">
                     <img className = "home--img" alt = "Wind Turbing Image" src = {HomepageImg} />
                 </div>
             </div>
-    </section>
+
+      </section>
     </Fragment>
 
 
