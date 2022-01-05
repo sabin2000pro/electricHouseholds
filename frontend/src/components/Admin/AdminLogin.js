395ae586fea9c0023198f8d919fed57b7b81a862
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-05 13:53:38 +0000

    Adding links with React Router

--- frontend/src/components/Admin/AdminLogin.js
+++ frontend/src/components/Admin/AdminLogin.js
@@ -1,9 +1,10 @@
-import React, {useState, useRef, useReducer, useContext} from 'react';
+import React, {useState, useRef, useReducer, useContext, useEffect} from 'react';
 import './AdminLogin.css';
 
 const AdminLogin = (props) => { // Admin Login Component
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+    const [formValid, setFormValid] = useState(true);
     
     return (
         <div>
