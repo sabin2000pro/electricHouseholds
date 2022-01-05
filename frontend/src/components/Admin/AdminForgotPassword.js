0268ce3118c99c604bffe547a444e3617c806b6a
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-05 15:37:30 +0000

    Adding internal commentary

--- frontend/src/components/Admin/AdminForgotPassword.js
+++ frontend/src/components/Admin/AdminForgotPassword.js
@@ -1,15 +1,8 @@
 import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 
-const emailReducer = (state, action) => { // E-mail Reducer that shares state for the email and valid
-     if(action.type === 'EMAIL_SUBMIT') {
-
-     }
-
-     return {value: '', emailIsValid: false};
-}
-
 const forgotPasswordSubmitHandler = (event) => {
+
     event.preventDefault();
 
     try {
@@ -28,15 +21,13 @@ const forgotPasswordSubmitHandler = (event) => {
 const AdminForgotPassword = (props) => {
     let history = useHistory();
 
-    const [email, setEmailAddress] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
     const [emailValid, setEmailValid] = useState(false);
     const [formIsValid, setFormIsValid] = useState(false);
 
     return (
         <Fragment>
-            <form onSubmit = {forgotPasswordSubmitHandler}>
-
-            </form>
+           
         </Fragment>
     )
 }
