98af90bdaee05e3bdcb3bcb64f0a108e505363b2
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-05 22:06:53 +0000

    Adding Contact Us backend

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,1 +1,32 @@
 const Contact = require('../models/contactModel');
+const catchAsync = require('../utils/catchAsync');
+const ok = 200;
+const created = 201;
+const notFound = 204;
+const serverError = 500;
+const badRequest = 400;
+
+
+module.exports.getAllContacts = catchAsync(async (request, response, next) => {
+    
+});
+
+module.exports.getContactByID = catchAsync(async (request, response, next) => {
+
+});
+
+module.exports.createContact = catchAsync(async (request, response, next) => {
+
+});
+
+module.exports.editContact = catchAsync(async (request, response, next) => {
+
+});
+
+module.exports.deleteAllContacts = catchAsync(async (request, response, next) => {
+
+});
+
+module.exports.deleteContactByID = catchAsync(async (request, response, next) => {
+
+})
