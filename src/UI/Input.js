ce725524fbbfdf373b262b4e73fa8ed09b0d3a1b
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-06 14:36:18 +0000

    Adding POST logic to contact form

--- src/UI/Input.js
+++ src/UI/Input.js
@@ -3,7 +3,7 @@ import React, {Fragment} from 'react'
 const Input = (props) => { // Input Component
     return (
          <Fragment>
-             <input onBlur = {props.onBlurHandler} type = {props.type} id = {props.id} value = {props.value} />
+             <input onBlur = {props.onBlurHandler} type = {props.type} id = {props.id} value = {props.value} placeholder = {props.placeholder} />
          </Fragment>
     )
 }
