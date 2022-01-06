40fc61ed69094850f1efa368293638a9868b6462
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-06 12:24:30 +0000

    Adding CRUD operations for the Contact us form

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const contactRouter = express.Router();
+const contactController = require('../controllers/contactController');
+
+contactRouter.route('/create-contact').post(contactController.createContact);
+contactRouter.route('/fetch-contacts').get(contactController.getAllContacts);
+contactRouter.route('/fetch-contacts/:id').get(contactController.getContactByID);
+contactRouter.route('/edit-contact/:id').put(contactController.editContact);
+contactRouter.route('/delete-contacts').delete(contactController.deleteAllContacts);
+contactRouter.route('/delete-contact/:id').delete(contactController.deleteContactByID);
 
 module.exports = contactRouter;
