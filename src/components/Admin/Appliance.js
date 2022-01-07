3439adc0e770480f6048bc4b21dbc5eab48ce420
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2022-01-07 22:26:39 +0000

    Adding logic to reset admin password

---
+++ src/components/Admin/Appliance.js
@@ -0,0 +1,16 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types'
+
+
+const Appliance = (props) => {
+    return (
+        <Fragment>
+            <div>
+                
+            </div>
+        </Fragment>
+    )
+}
+
+export default Appliance
