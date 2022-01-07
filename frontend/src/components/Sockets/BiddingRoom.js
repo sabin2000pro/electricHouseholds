b10c9a2ba01d3d6c7162f6768581145832e94c42
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-07 13:50:48 +0000

    Adding logic to the Contact submission form

--- frontend/src/components/Sockets/BiddingRoom.js
+++ frontend/src/components/Sockets/BiddingRoom.js
@@ -1,7 +1,6 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import PropTypes from 'prop-types'
 
-
 const BiddingRoom = (props) => { // Bidding Room Component that prompts user to enter their username and button to join the bidding room
     return (
         <Fragment>
