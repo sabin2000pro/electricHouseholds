d719fdb9a4f724cb2cc31e44d8dcd4fa5c186cec
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-10 14:06:13 +0000

    Adding backend code to protect routes

--- backend/middleware/protectRoutes.js
+++ backend/middleware/protectRoutes.js
@@ -1,8 +1,31 @@
 
+const Admin = require('../models/adminModel');
+const ErrorResponse = require('../utils/errorResponse');
+const jwt = require('jsonwebtoken');
+
 module.exports.protect = (request, response, next) => {
+    let token;
+    const authHeader = request.headers.authorization;
+
+    if(authHeader && authHeader.startsWith('Bearer')) {
+        token = authHeader.split(' ')[1]; // Turn it into an array
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+
+        request.admin = await Admin.findById(decoded.id);
+        return next();
+    } 
     
+    catch(err) {
+        if(err) {
+            return next(new ErrorResponse(`Invalid Token.`, 401));
+        }
+    }
 };
 
 module.exports.restrictTo = (...roles) => {
-    
+
 }
