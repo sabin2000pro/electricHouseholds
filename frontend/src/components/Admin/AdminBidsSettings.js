b62cb196d8b941e208f8a3a1e3a0e770246590e6
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-11 10:41:58 +0000

    Adding frontend code to create appliance data

---
+++ frontend/src/components/Admin/AdminBidsSettings.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import Header from '../Header';
+import HomepageImg from '../images/homepage/homepageimg.jpg';
+import { useHistory } from 'react-router';
+
+const AdminBidsSettings = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AdminBidsSettings
