bd33bb1c041a829495a4ecd9b58fbb1019cfcc16
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-11 10:30:18 +0000

    Adding frontend code to create appliance data

--- src/components/Admin/AdminEditAppliance.js
+++ src/components/Admin/AdminEditAppliance.js
@@ -1,12 +1,46 @@
 import React, {Fragment} from 'react'
+import Header from '../Header';
+import {useHistory} from 'react-router-dom';
+import HomepageImg from '../images/homepage/homepageimg.jpg';
 
 const AdminEditAppliance = (props) => {
+    let history = useHistory();
+
+    const logoutHandler = () => { // Logout Handler Function to logout admins
+        localStorage.removeItem("authToken"); // Remove auth token from local storage
+        history.push('/admin-login'); // Redirect to Login
+        
+        return window.location.reload(false);
+    }
+
     return (
         <Fragment>
-            <div>
-
-            </div>
-        </Fragment>
+        <Header />
+          
+          <section className = "section--home">
+              <div className = "home--grid">
+  
+          <div className = "home-text-box">
+  
+          <h1 className = "heading--primary">Edit Appliance</h1>
+          <p className = "home--description">Welcome to your Admin Dashboard. Here you will be able to view all of the electrical appliances available that users can submit their preferences for. You have the option to search for appliances if there are too many as well.</p>
+  
+          <a className = "btn btn--full mgr-sm" href = "/admin-dashboard">Admin Home</a>
+          <a onClick = {logoutHandler} className = "btn btn--outline" href = "/admin-login">Logout</a>
+  
+          </div>
+  
+          <div className = "home-img-box">
+              <img className = "home--img" alt = "Wind Turbine" src = {HomepageImg} />
+          </div>
+  
+  </div>
+  
+      </section>
+  
+     
+      
+          </Fragment>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -42,7 +42,7 @@ const Header = (props) => { // Header Component
 
                         <li><a className = "main-nav--link" href = "/admin-dashboard">Admin Dashboard</a></li>
                         <li><a className = "main-nav--link" href = "/admin-dashboard/create-appliance">Create Appliance</a></li>
-                        <li><a className = "main-nav--link" href = "/admin-dashboard/edit-appliances">Edit Appliance</a></li>
+                        <li><a className = "main-nav--link" href = "/admin-dashboard/edit-appliance">Edit Appliance</a></li>
                 </ul>
             </nav>
             </header>) : (<header className = "header">
