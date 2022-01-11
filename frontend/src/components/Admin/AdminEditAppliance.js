1aa2d1041c061785a2da3d5e7adf6f8eb10cd482
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-11 10:02:26 +0000

    Adding homepage to the create appliance admin

--- frontend/src/components/Admin/AdminEditAppliance.js
+++ frontend/src/components/Admin/AdminEditAppliance.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
-const AdminEditAppliance = () => {
+const AdminEditAppliance = (props) => {
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <div>
+                
+            </div>
+        </Fragment>
     )
 }
 
