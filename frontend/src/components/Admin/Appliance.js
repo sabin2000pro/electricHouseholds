87832cd5f6c93cdd6633c17dc8b3dce56acd9c96
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 15:53:45 +0000

    Adding code to view details about a single appliance

--- frontend/src/components/Admin/Appliance.js
+++ frontend/src/components/Admin/Appliance.js
@@ -2,7 +2,8 @@ import React, {Fragment, useState, useEffect} from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Header from '../Header';
-import HomepageImg from '../images/homepage/homepageimg.jpg'
+import HomepageImg from '../images/homepage/homepageimg.jpg';
+import RegisterCard from './RegisterCard';
 
 const Appliance = (props) => {
     let history = useHistory();
@@ -10,7 +11,7 @@ const Appliance = (props) => {
     const {name, image, description} = location.state.appliance;
     
     return (
-        <Fragment>
+    <Fragment>
         <Header />
 
         <section className = "section--home">
@@ -22,15 +23,46 @@ const Appliance = (props) => {
                 <p className = "home--description">View information about appliance below</p>
 
                 <a className = "btn btn--full mgr-sm" href = "/your-preferences">Start Now</a>
-                <a className = "btn btn--outline" href = "/about-us">About Us</a>
+                <a className = "btn btn--outline" href = "/home">Logout</a>
             </div>
 
                 <div className = "home-img-box">
                     <img className = "home--img" alt = "Wind Turbing On The Main Webpage" src = {HomepageImg} />
                 </div>
-
             </div>
     </section>
+
+    <section className = "section--login">
+
+        <div className = "container grid grid--2-cols">
+
+                <RegisterCard>
+                    <h1 className = "heading--primary login">{name}</h1>
+
+                 
+                 <div className = "login--form">
+
+                    <img style = {{marginLeft: '250px', marginRight: '-150px'}} src = {image} width = "300" height = "280" />
+            
+
+                    <div className = "applianceimage--box">
+                        <h1 style = {{textAlign: 'center', marginLeft: '200px', color: 'white', fontSize: '32px'}}>{description}</h1>
+                    </div>
+</div>
+                   
+                
+            </RegisterCard>
+            
+        </div>    
+    </section>
+
+    <footer className = "footer">
+        <ul className = "footer--items">
+            <li className = "footer--item">Copyright All Rights Reserved - eHouseholds Sabin Constantin Lungu - 2021</li>
+        </ul>
+    </footer>
+
+
     </Fragment>
 
     )
