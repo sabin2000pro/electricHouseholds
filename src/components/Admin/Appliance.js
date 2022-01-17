74b59933981ff10fe247d282c55c04b8c6c03baf
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 15:28:07 +0000

    Adding code to view details about a single appliance

--- src/components/Admin/Appliance.js
+++ src/components/Admin/Appliance.js
@@ -1,16 +1,38 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types'
+import Header from '../Header';
+import HomepageImg from '../images/homepage/homepageimg.jpg'
 
 const Appliance = (props) => {
     let history = useHistory();
+    let location = useLocation();
+    const {name, image, description} = location.state.appliance;
     
     return (
         <Fragment>
-            <div>
+        <Header />
+
+        <section className = "section--home">
+                <div className = "home--grid">
+
+            <div className = "home-text-box">
+
+                <h1 className = "heading--primary">Your Appliance</h1>
+                <p className = "home--description">View information about appliance below</p>
+
+                <a className = "btn btn--full mgr-sm" href = "/your-preferences">Start Now</a>
+                <a className = "btn btn--outline" href = "/about-us">About Us</a>
+            </div>
+
+                <div className = "home-img-box">
+                    <img className = "home--img" alt = "Wind Turbing On The Main Webpage" src = {HomepageImg} />
+                </div>
 
             </div>
-        </Fragment>
+    </section>
+    </Fragment>
+
     )
 }
 
