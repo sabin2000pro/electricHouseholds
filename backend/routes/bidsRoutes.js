56aa23a1663541fec48f672b02777c8617574d93
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 10:01:32 +0000

    Adding express routes to the bid backend and feedbacks

--- backend/routes/bidsRoutes.js
+++ backend/routes/bidsRoutes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const bidRouter = express.Router();
 const bidsController = require('../controllers/bidsController'); // Import the bids controller
 
+bidRouter.route('/fetch-bids').get(bidsController.getAllBids);
+bidRouter.route('/create-bid').post(bidsController.createBid);
+bidRouter.route('/delete-bids').delete(bidsController.deleteAllBids);
+
 module.exports = bidRouter;
