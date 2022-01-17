71eccf016c06a08d516e62a56a80c2cc1ae6c611
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 15:55:50 +0000

    Adding code to edit an appliance

--- src/components/Admin/EditAppliance.js
+++ src/components/Admin/EditAppliance.js
@@ -1,9 +1,11 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 import axios from 'axios';
 
 const EditAppliance = () => {
     let history = useHistory();
+    let location = useLocation();
+    const {name, image, description} = location.state.appliance;
 
     return (
     <Fragment>
