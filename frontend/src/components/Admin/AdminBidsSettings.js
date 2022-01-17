66abe7fa6f6a15c0e3946e433bbcd3f74f275912
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 13:41:47 +0000

    Adding code to display all appliances to the admin dashboard home

--- frontend/src/components/Admin/AdminBidsSettings.js
+++ frontend/src/components/Admin/AdminBidsSettings.js
@@ -1,11 +1,12 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from '../Header';
 import {useHistory, useLocation} from 'react-router-dom';
 import HomepageImg from '../images/homepage/homepageimg.jpg';
+import RegisterCard from './RegisterCard';
 
 const AdminBidsSettings = () => {
     let history = useHistory();
-
+    const [enteredNickname, setEnteredNickname] = useState('');
 
     const logoutHandler = () => { // Logout Handler Function to logout admins
         localStorage.removeItem("authToken"); // Remove auth token from local storage
@@ -13,7 +14,6 @@ const AdminBidsSettings = () => {
         
         return window.location.reload(false);
     }
-
    
 
     return (
@@ -37,10 +37,46 @@ const AdminBidsSettings = () => {
             <img className = "home--img" alt = "Wind Turbine" src = {HomepageImg} />
         </div>
         </div>
-    
     </section>
 
+    <section className = "section--login">
+
+        <div className = "container grid grid--2-cols">
+
+                <RegisterCard>
+                    <h1 className = "heading--primary login">Configure Bid Settings</h1>
+
+                    <form className = "login--form">
+
+                    
+                    <div className = "appliancename--box">
+                        <label className = "name--lbl">Name</label>
+                        <input placeholder = "Enter Appliance Name" type = "text"/>
+                    </div>
+
+                    <div className = "applianceimage--box">
+                        <label className = "image--lbl">Image</label>
+                        <input placeholder = "Enter Appliance Image URL" required id = "applianceurl" type = "text"/>
+                    </div>
+
+                    <div className = "appliancedescription--box">
+                        <label className = "description--lbl">Description</label>
+                        <input  placeholder = "Enter Appliance Description" required id = "description" type = "text"/>
+                    </div>
+                    
+                    <div className = "submit--container">
+                        <button className = "submit--btn" type = "submit">Submit</button>
+                    </div>
+
+                    </form>
+                
+            </RegisterCard>
+            
+        </div>    
+    </section>
+  
 </Fragment>
+
     )
 }
 
