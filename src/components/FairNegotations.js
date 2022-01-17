fecf99aefc53065e75cd90af75d1aa8c448d3348
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-17 14:26:34 +0000

    Adding code to view a single appliance and a single preference

---
+++ src/components/FairNegotations.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect, Fragment} from 'react';
+import {useHistory, useLocation} from 'react-router-dom';
+import Header from './Header';
+import RegisterCard from './Admin/RegisterCard';
+
+const FairNegotations = (props) => {
+    return (
+        <Fragment>
+            <div>
+
+            </div>
+        </Fragment>
+    )
+}
+
+export default FairNegotations
