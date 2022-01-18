d5c878ba531e195db8ae5be0980a8c4f3479df9e
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 14:40:36 +0000

    Adding auth rate limiting

--- backend/routes/applianceRoutes.js
+++ backend/routes/applianceRoutes.js
@@ -9,11 +9,11 @@ const applianceLimiter = rateLimit({
     message: "Too many appliance requests coming from this IP address. Try again after 30 minutes. Only 10 per 30 minutes"
 });
 
-applianceRouter.route('/fetch-appliances').get(applianceController.getAllAppliances);
-applianceRouter.route('/create-appliance').post(applianceController.createAppliance);
-applianceRouter.route('/edit-appliance/:id').put(applianceController.editAppliance);
-applianceRouter.route('/fetch-single-appliance/:id').get(applianceController.getApplianceByID);
-applianceRouter.route('/delete-appliances').delete(applianceController.deleteAppliances);
-applianceRouter.route('/delete-appliance/:id').delete(applianceController.deleteAppliance);
+applianceRouter.route('/fetch-appliances').get(applianceLimiter, applianceController.getAllAppliances);
+applianceRouter.route('/create-appliance').post(applianceLimiter, applianceController.createAppliance);
+applianceRouter.route('/edit-appliance/:id').put(applianceLimiter, applianceController.editAppliance);
+applianceRouter.route('/fetch-single-appliance/:id').get(applianceLimiter, applianceController.getApplianceByID);
+applianceRouter.route('/delete-appliances').delete(applianceLimiter, applianceController.deleteAppliances);
+applianceRouter.route('/delete-appliance/:id').delete(applianceLimiter, applianceController.deleteAppliance);
 
 module.exports = applianceRouter; // Export the appliance router
