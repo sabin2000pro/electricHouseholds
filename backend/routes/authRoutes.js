b33929e47bd0db69e5732660de29a09634e40180
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 14:15:00 +0000

    Adding rate limiting to improve security of app

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const authRouter = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/protectRoutes')
+const authMiddleware = require('../middleware/protectRoutes');
 
-authRouter.route('/register-admin').post(authController.registerAdmin);
+const resetPasswordLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // Every 1 hour
+	max: 3, // Limit each IP to 3 reset password requests per `window` (here, per 1 hour)
+	message: 'Too many reset password requests coming from this IP, please try again after an hour',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const registerAccountLimiter = rateLimit({
+    windowMs: 40 * 40 * 1000,
+    max: 3,
+    message: "Too many register account requests coming from this IP address. Try again after 30 minutes. Only 3 per 30 minutes"
+})
+
+authRouter.route('/register-admin').post(registerAccountLimiter, authController.registerAdmin);
 authRouter.route('/login-admin').post(authController.loginAdmin);
 authRouter.route('/forgot-password').post(authController.forgotPassword);
-authRouter.route('/admin/reset-password/:resetToken').put(authController.resetAdminPassword);
+authRouter.route('/admin/reset-password/:resetToken').put(resetPasswordLimiter, authController.resetAdminPassword);
 authRouter.route('/fetch-admins').get(authController.fetchAllAdmins);
 authRouter.route('/me').get(authMiddleware.protect, authController.getMe);
 
