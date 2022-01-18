ca7709e38e77745b4d173dcd42b64388a3235eb8
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 14:59:53 +0000

    Adding code to the seeder fiel to import admin accounts to DB

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+// Configure Environment Variables
+const dotenv = require('dotenv');
+dotenv.config({path: 'config.env'});
+const DB_URL = process.env.DB_CONN_URL;
+
+// Connect to database
+mongoose.connect(DB_URL, {
+    
+})
+
+
+
+
+
+
+// Command Line Arguments for manipulating data
+if(process.argv[2] === '--import') {
+
+};
+
+if(process.argv[2] === '--delete') {
+    
+}
