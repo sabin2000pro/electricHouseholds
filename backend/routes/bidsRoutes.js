3960638041af602b98371e130050c208426be151
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 14:19:40 +0000

    Adding rate limiting to improve security of app

--- backend/routes/bidsRoutes.js
+++ backend/routes/bidsRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bidRouter = express.Router();
+const rateLimit = require('express-rate-limit');
 const bidsController = require('../controllers/bidsController'); // Import the bids controller
 
-bidRouter.route('/fetch-bids').get(bidsController.getAllBids);
+const bidLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 50,
+    message: "Too many bids coming from this IP address. Try again after 1 hour. Only 50 per hour"
+});
+
+bidRouter.route('/fetch-bids').get(bidLimiter, bidsController.getAllBids);
 bidRouter.route('/fetch-openingbid/:id').get(bidsController.fetchOpeningBid);
 bidRouter.route('/fetch-bid/:id').get(bidsController.fetchSingleBid);
 bidRouter.route('/edit-bid/:id').put(bidsController.editBids);
-bidRouter.route('/create-bid').post(bidsController.createBid);
+bidRouter.route('/create-bid').post(bidLimiter, bidsController.createBid);
 bidRouter.route('/delete-bids').delete(bidsController.deleteAllBids);
 
 module.exports = bidRouter;
