2318b51e8abd760742b10e0a9e8b4c27a329ee2b
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 15:16:04 +0000

    Finishing code for the seeder

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const feedbackRouter = express.Router();
 const feedbackController = require('../controllers/feedbackController');
+const rateLimit = require('express-rate-limit');
 
 feedbackRouter.route('/fetch-feedbacks').get(feedbackController.getAllFeedbacks);
 feedbackRouter.route('/create-feedback').post(feedbackController.createFeedback);
