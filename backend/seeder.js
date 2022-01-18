da9242adbdd2eca80af1bdd63ba56f4d61e74223
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-18 15:02:40 +0000

    Adding code to the seeder fiel to import admin accounts to DB

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,17 +1,48 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
 // Configure Environment Variables
 const dotenv = require('dotenv');
 dotenv.config({path: 'config.env'});
 const DB_URL = process.env.DB_CONN_URL;
+const Admin = require('./models/adminModel');
+
+let errPresent;
 
 // Connect to database
 mongoose.connect(DB_URL, {
     
-})
+});
 
 
+const admins = JSON.parse(fs.readFileSync(path.join(`${__dirname}/data/admins.json`), 'utf-8'));
 
 
+const importData = async () => {
+    try {
+        // Load JSON file into the DB model
+    } 
+    
+    catch(error) {
+        if(error) {
+            errPresent = true;
+            return console.error(error);
+        }
+    }
+};
+
+const removeData = async () => {
+    try {
+
+    } 
+    
+    catch(error) {
+        if(error) {
+            errPresent = true;
+            return console.error(error);
+        }
+    }
+}
 
 
 // Command Line Arguments for manipulating data
@@ -20,5 +51,5 @@ if(process.argv[2] === '--import') {
 };
 
 if(process.argv[2] === '--delete') {
-    
+
 }
