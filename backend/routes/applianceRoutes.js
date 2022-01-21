81d8202a2b56827f00d0b848307a2e9fee934343
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-21 20:10:42 +0000

    Adding code to seed the database

--- backend/routes/applianceRoutes.js
+++ backend/routes/applianceRoutes.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const applianceRouter = express.Router();
 const applianceController = require('../controllers/applianceController');
-const rateLimit = require('express-rate-limit');
 
-const applianceLimiter = rateLimit({
-    windowMs: 40 * 40 * 1000,
-    max: 10,
-    message: "Too many appliance requests coming from this IP address. Try again after 30 minutes. Only 10 per 30 minutes"
-});
-
-applianceRouter.route('/fetch-appliances').get(applianceLimiter, applianceController.getAllAppliances);
-applianceRouter.route('/create-appliance').post(applianceLimiter, applianceController.createAppliance);
-applianceRouter.route('/edit-appliance/:id').put(applianceLimiter, applianceController.editAppliance);
-applianceRouter.route('/fetch-single-appliance/:id').get(applianceLimiter, applianceController.getApplianceByID);
-applianceRouter.route('/delete-appliances').delete(applianceLimiter, applianceController.deleteAppliances);
-applianceRouter.route('/delete-appliance/:id').delete(applianceLimiter, applianceController.deleteAppliance);
+applianceRouter.route('/fetch-appliances').get(applianceController.getAllAppliances);
+applianceRouter.route('/create-appliance').post(applianceController.createAppliance);
+applianceRouter.route('/edit-appliance/:id').put(applianceController.editAppliance);
+applianceRouter.route('/fetch-single-appliance/:id').get(applianceController.getApplianceByID);
+applianceRouter.route('/delete-appliances').delete(applianceController.deleteAppliances);
+applianceRouter.route('/delete-appliance/:id').delete(applianceController.deleteAppliance);
 
 module.exports = applianceRouter; // Export the appliance router
