088bc6fbacea0a0ea3ad5bc28556d46b7c2d15a1
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-01-25 14:23:02 +0000

    Adding AI Bot CRUD functionality to the backend

--- src/components/AIBot/BotSettings.js
+++ src/components/AIBot/BotSettings.js
@@ -1,4 +1,6 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
+import RegisterCard from '../Admin/RegisterCard';
+import ModalCard from '../../UI/ModalCard';
 
 const BotSettings = (props) => {
   return <Fragment>
