c1889531643bb1072e54138b328fb5e89c17ce8f
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-02-01 08:38:29 +0000

    Configuring the bot settings in the admin Dashboard

--- backend/controllers/botController.js
+++ backend/controllers/botController.js
@@ -3,31 +3,37 @@ const catchAsync = require('../utils/catchAsync');
 const ok = 200;
 const created = 201;
 const noContent = 204;
+const notFound = 404;
 
 module.exports.getAllBots = catchAsync(async(request, response, next) => {
     const allBots = await Bot.find();
-    return response.status(200).json({allBots});
+    return response.status(ok).json({allBots});
 });
 
 module.exports.getSingleBot = catchAsync(async(request, response, next) => {
-    const id = request.params.id;
-    const bot = await Bot.findById(id);
 
-    if(!bot) {
-        return response.status(404).json({status: "Fail", message: 'Bot not found'});
+    if(request.method === 'GET') {
+        const id = request.params.id;
+        const bot = await Bot.findById(id);
+
+        return response.status(ok).json({bot});
     }
 
-    return response.status(200).json({bot});
+    if(!bot) {
+        return response.status(notFound).json({status: "Fail", message: 'Bot not found'});
+    }
+
 });
 
 module.exports.createBot = catchAsync(async (request, response, next) => {
-    const {name, botCredits, type} = request.body;
+    const {name, botCredits, type, numberOfBots} = request.body;
 
-    const newBot = new Bot({name, botCredits, type});
-    await newBot.save();
+    if(request.method === 'POST') {
 
-    return response.status(created).json({newBot});
-
+        const newBot = new Bot({name, botCredits, type, numberOfBots});
+        await newBot.save();
+        return response.status(created).json({newBot});
+    }
 });
 
 module.exports.editBot = catchAsync(async (request, response, next) => {
@@ -44,16 +50,16 @@ module.exports.deleteBot = catchAsync(async (request, response, next) => {
     const id = request.params.id;
 
     if(request.method === 'DELETE') {
-        await Bot.findByIdAndDelete(id);
-        return response.status(204).json({status: "Success", message: "Bot deleted success"});
-    }  
 
+        await Bot.findByIdAndDelete(id);
+        return response.status(noContent).json({status: "Success", message: "Bot deleted success"});
+    }  
 
 });
 
 module.exports.deleteAllBots = catchAsync(async (request, response, next) => {
     if(request.method === 'DELETE') {
         await Bot.deleteMany();
-        return response.status(204).json("Bots Deleted");
+        return response.status(noContent).json("Bots Deleted");
     }
 });
