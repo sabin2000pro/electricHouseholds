d7eaddef0e687b4fd9552933e1049ddd80d4d785
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-02-01 10:30:05 +0000

    Adding code to submit feedback:

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const feedbackRouter = express.Router();
 const feedbackController = require('../controllers/feedbackController');
-const rateLimit = require('express-rate-limit');
 
 feedbackRouter.route('/fetch-feedbacks').get(feedbackController.getAllFeedbacks);
 feedbackRouter.route('/create-feedback').post(feedbackController.createFeedback);
