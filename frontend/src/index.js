3e1ff4d1a5b8b43188faaee97b5d1bf2ac47274d
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-08 22:56:54 +0000

    Added browser router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App'
-import './index.css'
+import './index.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App />  </BrowserRouter>,document.getElementById('root'));
