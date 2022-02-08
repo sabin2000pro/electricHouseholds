10f44a147d0cbbc7a7401b161c200fb72ca9fa13
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-08 23:21:29 +0000

    FINAL TIME

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ const App = () => { // Trying to add the permissions.. Added browser router//.. 
 
     <div className = "App">
 
-      <BrowserRouter basename={window.location.pathname || ''}>
+      <BrowserRouter>
         <Route exact path = '/' component={Homepage} />
         <Route exact path = '/api/v1/auth/client/admin-register' component = {AdminRegister}/>
         <Route exact path = '/your-preferences' component = {PreferencesHome}/>
