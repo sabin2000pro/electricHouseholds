c0621411077b4e5abbc813fc425520fdaa71c95a
Sabin <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-02-09 08:49:04 +0000

    Added BASE URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ const App = () => { // Trying to add the permissions.. Added browser router//.. 
 
     <div className = "App">
 
-      <BrowserRouter basename= '/electricHouseholds'>
+      <BrowserRouter basename= {process.env.PUBLIC_URL}>
 
       <Switch>
       <Route exact path = '/' component={Homepage} />
