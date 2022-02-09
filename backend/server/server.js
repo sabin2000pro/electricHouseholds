f9c2b0cb9068b269a3bd61ccb03acdf91b9aa170
Sabin <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-02-09 09:59:28 +0000

    Added some hTTP headers

--- backend/server/server.js
+++ backend/server/server.js
@@ -37,24 +37,6 @@ app.use(nocache());
 app.use(express.json());
 connectDB();
 
-app.use(function (req, res, next) {
-
-   
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5200');
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-   
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-   
-    
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-   
-    next();
-});
-
 // Mount the routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/appliances', applianceRoutes);
