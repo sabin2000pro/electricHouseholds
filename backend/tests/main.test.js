73e3b2f5a1ea11bc50fc0bf464e52d8f596f2761
Sabin <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-02-09 10:47:13 +0000

    Adding unit tests

--- backend/tests/main.test.js
+++ backend/tests/main.test.js
@@ -9,11 +9,42 @@ beforeAll(async() => { // Test DB connection before the tests
 
 describe('Authentication Test Suite', () => {
 
-   
+    test("Register New Admin Account. Should respond with a 201 created", async () => {
+        const adminBodyData = [{username: "testadmin00", emailAddress: "testadmin00@gmail.com", password: "test123", confirmPassword: "test123"}]
 
+        for(const bodyData of adminBodyData) {
+            const response = await request(server).post('/api/v1/auth/register-admin').send(bodyData);
+
+            return expect(response.statusCode).toBe(201);
+        }
+    });
+
+    test("Admin Login. Should Respond with 200 OK Code", async () => {
+        const adminLoginData = [{emailAddress: "testadmin00@gmail.com", password: "test123"}]
+
+        for(const loginData of adminLoginData) {
+            const response = await request(server).post('/api/v1/auth/login-admin').send(loginData);
+
+            return expect(response.statusCode).toBe(200);
+        }
+    });
+
+    test("Admin Forgot Password. Should Respond with a 200 OK Status Code", async () => {
+         const forgotBodyData = [{emailAddress: "testadmin00@gmail.com"}];
+
+         for(const bodyData of forgotBodyData) {
+             const resonse = await request(server).post('/api/v1/auth/forgot-password').send(bodyData);
+
+             return expect(respoonse.statusCode).toBe(200);
+         }
+    })
+
+
+
+   
     // Test Case 2 - Admin Register Invalid Data
     test('Register Admin with Invalid Data. Should respond with 400 Bad Request', async () => {
-        const invalidBodyData = [{username: "0", emailAddress: "122239jdf", password: '1'}];
+        const invalidBodyData = [{username: "0", emailAddress: "admin2", password: '123'}];
 
         for(const bodyData of invalidBodyData) {
             const response = await request(server).post('/api/v1/auth/register-admin').send(bodyData);
@@ -21,36 +52,9 @@ describe('Authentication Test Suite', () => {
         }
     })
 
-    test('Fetch All Admins. Should respond with a 200 OK status code', async () => {
-        const theResponse = await request(server).get('/api/v1/auth/fetch-admins').send();
-        return expect(theResponse.statusCode).toBe(200);
-    });
 
-   
 
-    test('Login Admin - Invalid E-mail Address and Invalid Password', async () => {
-        const invalidLoginData = [{username: "unknown", password: "unknown"}];
 
-        for(const loginData of invalidLoginData) {
-            const serverResponse = await request(server).post('/api/v1/auth/login-admin').send(loginData);
-            return expect(serverResponse.statusCode).toBe(401)
-        }
-    });
-
-    test('Login Admin - Missing Password', async () => {
-        const missingPassword = [{password: ''}];
-
-        for(const data of missingPassword) {
-            const response = await request(server).post('/api/v1/auth/login-admin').send(data);
-            return expect(response.statusCode).toBe(401);
-        }
-    });
-
-   
-
-   
-
-  
 
     // Test Suite 2
 describe("Appliances Test Suite. - CRUD Operations", () => {
@@ -65,110 +69,4 @@ describe("Appliances Test Suite. - CRUD Operations", () => {
         }
     });
 
-    test('Fetch All Appliances. Should return a 200 OK Status Code with list of appliances created', async () => {
-        const response = await request(server).get('/api/v1/appliances/fetch-appliances');
-        return expect(response.statusCode).toBe(200);
-    });
-
-    test('Edit Appliance By ID. Should return a 200 OK status code', async () => {
-        const editBodyData = [{}]
-    });
-
-    test('Delete All Appliances. Should return a 204 NO CONTENT status code', async () => {
-        const response = await request(server).delete('/api/v1/appliances/delete-appliances');
-        return expect(response.statusCode).toBe(204);
-    });
-
-    test('Delete Appliance by Single ID. Should return a 201 CREATED Status Code', async () => {
-
-    })
-
-    test('Sort all appliances in ascending order. Should return with a 200 OK Status code', async () => {
-
-    })
-
-    describe('User Preferences Testing Suite.', () => {
-
-        // Test Case 1 for User Preferences - POST Request
-    
-      
-        // Test Case 2 - GET Request
-        test('Fetch All User Preferences', async () => {
-            const response = await request(server).get('/api/v1/preferences/fetch-preferences');
-            return expect(response.statusCode).toBe(200);
-        });
-
-        test("Delete all user preferences", async () => {
-            const response = await request(server).delete('/api/v1/preferences/delete-preferences');
-            return expect(response.statusCode).toBe(204);
-        });
-    })
-});
-});
-
-describe('User Comments - Testing Suite', () => {
-
-    test('View All Comments - Should return 200 OK status Code', async () => {
-
-    });
-
-    test('Create Single Comment - Should return 201 Created Status Code', async () => {
-
-    });
-
-    test('Create Single Comment with Invalid Data - Should return with a 400 Bad Request ', async () => {
-
-    });
-
-    test('Delete Single Comment - Should return 204 No Content Code', async () => {
-
-    });
-})
-
-describe('Algorithms Review - Testing Suite', () => {
-    test('View All Reviews. Should Return a 200 OK Status Code', async () => {
-
-    })
-
-    test('Create Review. Should return a 201 Created Status Code', async () => {
-
-    });
-
-    test('Create Invalid Review with missing data. Should return a 404 not found code', async () => {
-
-    });
-
-    test('Create Invalid Review with wrong data types. Should return a 400 Bad Request Data Type', async () => {
-
-    })
-})
-
-describe('Contact Us - Testing Suite', () => {
-    test('Fetch All Contacts', async () => {
-
-    });
-
-    test('Create Contact With Valid Data. Should return a 201 Status Code', async () => {
-
-    })
-
-    test('Create Contact with Missing Data. Should return a 404 status code', async () => {
-
-    });
-
-    test('Delete all contacts. Should return a 204 status code', async () => {
-        
-    })
-})
-
-describe('Feedback - Testing Suite', () => {
-    test('Fetch All Feedbacks. Should return a 200 Status Code', async () => {
-
-    });
-
-    test('Create Feedback. Should return a 201 Status Code', async () => {
-
-    });
-
-    
-})
+})})
