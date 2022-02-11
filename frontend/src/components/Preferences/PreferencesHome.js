cea02388321417fe86a9afb3773d5536c2f58a0f
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-11 19:46:20 +0000

    Adding instructions on the webpage

--- frontend/src/components/Preferences/PreferencesHome.js
+++ frontend/src/components/Preferences/PreferencesHome.js
@@ -17,7 +17,7 @@ const PreferencesHome = () => {
     
     
                 <h1 className = "heading--primary">Your Preferences</h1>
-            <p className = "home--description">Fill out the form below and let us know when you wish to run your electrical appliances. You can select up to three different timeslot preferences that you can submit that suits your needs. After you have created your preferences you can view them below. You will also be presented with randomly allocated timeslots which you can then negotiate using our English Auction Algorithm.</p>
+            <p className = "home--description">Fill out the form below and let us know when you wish to run your electrical appliances. You can select up to three different timeslot preferences that you can submit that suits your needs. <br/> <br/> After you have submitted your preferences you can view them below. You will also be presented with randomly allocated timeslots which you can then negotiate using our First Price Sealed Bid Algorithm.</p>
     
             <a className = "btn btn--full mgr-sm" href = "/your-preferences">Start Now</a>
             <a className = "btn btn--outline" href = "/about-us">About Us</a>
