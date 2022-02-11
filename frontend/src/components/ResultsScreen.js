8b03a8e87ca5d64aef2fcd439c76d467091345ad
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-11 17:22:18 +0000

    Adding results screen after win/loss

--- frontend/src/components/ResultsScreen.js
+++ frontend/src/components/ResultsScreen.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect, Fragment} from 'react';
 const ResultsScreen = (props) => { // The results screen that shows the results at the end of the bidding session rounds
   return <Fragment>
         <div>
-            
+            <h1>Your Results Here </h1>
         </div>
   </Fragment>
 };
