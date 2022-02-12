2228701c217cfcb85bba220209d27fbb75e40a3f
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-12 11:22:51 +0000

    Adding copyright, documentation to functions and removing unused code

--- frontend/src/components/Admin/AdminCreateAppliance.js
+++ frontend/src/components/Admin/AdminCreateAppliance.js
@@ -1,4 +1,16 @@
-import React, {useState, useRef, useContext, useReducer, Fragment} from 'react';
+/*
+ * File Creation Date: December 2021
+ * Author: Sabin Constantin Lungu
+ * -----
+ * Last Modified: Saturday 12th February 2022
+ * Modified By: Sabin Constantin Lungu
+ * -----
+ * Copyright (c) 2021-2022 - eHouseholds Sabin Constantin Lungu - Edinburgh Napier Univeristy - All Rights Reserved
+ * Any unauthorised broadcasting, public performance, copying or re-recording will constitute an infringement of copyright
+ */
+
+
+import React, {useState, Fragment} from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../Header';
 import HomepageImg from '../images/homepage/homepageimg.jpg';
