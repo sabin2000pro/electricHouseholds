a4f002de0650ea896159abdd670026dce5cf6142
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-12 11:27:46 +0000

    Adding copyright, documentation to functions and removing unused code

--- frontend/src/components/Admin/AdminLogin.js
+++ frontend/src/components/Admin/AdminLogin.js
@@ -1,4 +1,16 @@
-import React, {useState, useRef, useReducer, useContext, useEffect, Fragment} from 'react';
+/*
+ * File Creation Date: December 2021
+ * Author: Sabin Constantin Lungu
+ * -----
+ * Last Modified: Saturday 12th February 2022
+ * Modified By: Sabin Constantin Lungu
+ * -----
+ * Copyright (c) 2021-2022 - eHouseholds Sabin Constantin Lungu - Edinburgh Napier Univeristy - All Rights Reserved
+ * Any unauthorised broadcasting, public performance, copying or re-recording will constitute an infringement of copyright
+ */
+
+
+import React, {useState, useEffect, Fragment} from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import './AdminLogin.css';
 import Header from '../Header';
@@ -30,13 +42,8 @@ const AdminLogin = (props) => { // Admin Login Component. Adding comments and up
          }
     }
 
-    const modalHandler = () => {
-        setModalShown(null);
-    }
-
     const missingEmail = <p className = "err-msg">Invalid E-mail Address</p>
     const notFound = <p className = "err-msg">Account not found</p>
-    const passwordInvalid = <p className = "err-msg">Password Invalid</p>
 
     const loginHandler = async (e) => { // Login Handler function to login admin
 
