1197fdfebe3ac5e4329db5cf613aef38ae7ead91
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-12 11:26:32 +0000

    Adding copyright, documentation to functions and removing unused code

--- frontend/src/components/Admin/AdminForgotPassword.js
+++ frontend/src/components/Admin/AdminForgotPassword.js
@@ -1,11 +1,21 @@
-import React, {useState, useEffect, useRef, useReducer, Fragment} from 'react';
-import {useHistory, useLocation, Link} from 'react-router-dom';
+/*
+ * File Creation Date: December 2021
+ * Author: Sabin Constantin Lungu
+ * -----
+ * Last Modified: Saturday 12th February 2022
+ * Modified By: Sabin Constantin Lungu
+ * -----
+ * Copyright (c) 2021-2022 - eHouseholds Sabin Constantin Lungu - Edinburgh Napier Univeristy - All Rights Reserved
+ * Any unauthorised broadcasting, public performance, copying or re-recording will constitute an infringement of copyright
+ */
+
+
+import React, {useState, Fragment} from 'react';
 import RegisterCard from './RegisterCard';
 import axios from 'axios';
 import './AdminForgotPassword.css'
 
 const AdminForgotPassword = ({match}) => { // Forgot Password Component
-    let history = useHistory();
 
     const [emailAddress, setEmailAddress] = useState('');
     const [emailValid, setEmailValid] = useState(false);
