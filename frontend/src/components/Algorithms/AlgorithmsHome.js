4ef8aaa915c1f214c18806653fe762e373f54b91
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-12 11:32:28 +0000

    Adding copyright, documentation to functions and removing unused code

--- frontend/src/components/Algorithms/AlgorithmsHome.js
+++ frontend/src/components/Algorithms/AlgorithmsHome.js
@@ -1,4 +1,16 @@
-import React, {useState, useEffect, Fragment, useReducer, useContext} from 'react';
+/*
+ * File Creation Date: December 2021
+ * Author: Sabin Constantin Lungu
+ * -----
+ * Last Modified: Saturday 12th February 2022
+ * Modified By: Sabin Constantin Lungu
+ * -----
+ * Copyright (c) 2021-2022 - eHouseholds Sabin Constantin Lungu - Edinburgh Napier Univeristy - All Rights Reserved
+ * Any unauthorised broadcasting, public performance, copying or re-recording will constitute an infringement of copyright
+ */
+
+
+import React, { Fragment} from 'react';
 import Header from '../../components/Header';
 import HomepageImg from '../../components/images/homepage/homepageimg.jpg';
 
