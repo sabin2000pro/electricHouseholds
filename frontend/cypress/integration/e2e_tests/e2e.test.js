19062a4d5da2ca6acd291ecb65c55b10469d3014
Sabin <sabin2000@SabinLungusMBP.broadband>
2022-02-27 15:21:37 +0000

    Adding E2E Cypress tests

--- frontend/cypress/integration/e2e_tests/e2e.test.js
+++ frontend/cypress/integration/e2e_tests/e2e.test.js
@@ -6,5 +6,9 @@ describe('First test', () => {
          cy.visit('http://localhost:3000/electricHouseholds');
      });
 
+     it('Scan Main Paragraph - Homepage', () => { // Test 1
+        
+     })
+
      
 })
