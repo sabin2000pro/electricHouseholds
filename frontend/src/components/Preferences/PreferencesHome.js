5ece876bf927e0344e7e33bc8b54b15a877155eb
Sabin <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2022-03-02 10:33:55 +0000

    Making changes to the text descriptions

--- frontend/src/components/Preferences/PreferencesHome.js
+++ frontend/src/components/Preferences/PreferencesHome.js
@@ -30,6 +30,7 @@ const PreferencesHome = () => {
     
                 <h1 className = "heading--primary">Your Preferences</h1>
             <p className = "home--description">Fill out the form below and let us know when you wish to run your electrical appliances. You can select up to three different timeslot preferences that you can submit that suits your needs. <br/> <br/> After you have submitted your preferences you can view them below. You will also be presented with randomly allocated timeslots which you can then negotiate using our First Price Sealed Bid Algorithm.</p>
+            <p className = "home--description">First-Priced Sealed Bid means that once you view your allocations and click on the view allocations button, you will be presented with an auction button that you can click on which begins the live auction.</p>
             <p className = "home--description">Make sure to reload the page after submitting your desired preferences</p>
 
     
