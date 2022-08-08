2da99726802398a9f59035525f351de93c99b5b0
sabin2000pro <sabinlungu293@gmail.com>
2022-08-08 20:22:21 +0100

    Rebuilding the images

--- backend/database/db.js
+++ backend/database/db.js
@@ -13,7 +13,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: '../config.env'})
-const DB_URL = process.env.DB_URL;
+const DB_URL = process.env.MONGO_URL
 
 const connectDB = async () => { // Method to connect to the MongoDB database
     
